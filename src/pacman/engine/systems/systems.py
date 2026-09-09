3d629794ef057dc08108d126478ab64dd8d8d982
Anselme Bounouar <abounoua@student.42lyon.fr>
2026-09-09 17:10:49 +0200

    feat: added collision system

--- src/pacman/engine/systems/systems.py
+++ src/pacman/engine/systems/systems.py
@@ -3,14 +3,16 @@ from abc import ABC, abstractmethod
 import pyray as pr
 
 pr.set_trace_log_level(pr.LOG_NONE)
-from pacman.engine.ecs import Component, Entity, Position, Sprite, Velocity
+from pacman.engine.ecs import Collision, Component, Entity, Hitbox, Position, Sprite, Velocity
 from pacman.services.sprites import SpriteService
+from typing import Callable, TypeVar
+import itertools
 
 
 class System(ABC):
     def __init__(self, components: list[type[Component]]):
         self.subscribers: list[Entity] = []
-        self.required_components: list[type[Component]] = []
+        self.required_components: list[type[Component]] = components
 
     def subscribe(self, entity: Entity):
         for component in self.required_components:
@@ -22,6 +24,49 @@ class System(ABC):
     @abstractmethod
     def run():
         ...
+
+
+class CollisionSystem(System):
+    def __init__(self, ressources: dict | None):
+        super().__init__([Position, Collision])
+        self.ressources = ressources
+        self.router_map: dict[
+            tuple[str, str], Callable[[Entity, Entity], None]
+        ] = {}
+
+    def router(self, first: str, second: str) -> Callable:
+        def marker(
+            func: Callable[[Entity, Entity], None]
+        ) -> Callable[[Entity, Entity], None]:
+            self.router_map[(first, second)] = func
+            self.router_map[(second, first)] = func
+            return func
+        return marker
+
+    def run(self) -> None:
+        for first, second in itertools.combinations(self.subscribers, 2):
+            f_pos = first.get_component(Position)
+            s_pos = second.get_component(Position)
+            f_hit = first.get_component(Hitbox)
+            s_hit = second.get_component(Hitbox)
+            f_col = first.get_component(Collision)
+            s_col = second.get_component(Collision)
+
+            #print(f"tag 1: {f_col.tag}, tag 2: {s_col.tag}")
+            if s_col.tag == f_col.tag:
+                continue
+
+            if (
+                f_pos.x + f_hit.width >= s_pos.x and
+                f_pos.x <= s_pos.x + s_hit.width and
+                f_pos.y + f_hit.height >= s_pos.y and
+                f_pos.y <= s_pos.y + s_hit.height
+            ):
+                if (f_col.tag, s_col.tag) in self.router_map:
+                    self.router_map[(f_col.tag, s_col.tag)](first, second)
+                elif (s_col.tag, f_col.tag) in self.router_map:
+                    self.router_map[(f_col.tag, s_col.tag)](first, second)
+
 
 class MovementSystem(System):
     def __init__(self, ressources: dict | None):
@@ -37,12 +82,12 @@ class MovementSystem(System):
             actu_position = subscriber.components[Position]
             velo = subscriber.components[Velocity]
     
-            print(f"Position actuelle {actu_position}")
+            #print(f"Position actuelle {actu_position}")
 
             actu_position.x += velo.x
             actu_position.y += velo.y
 
-            print(f"Nouvelle position: {actu_position}") 
+            #print(f"Nouvelle position: {actu_position}") 
 
 
 class SpriteSystem(System):
@@ -53,7 +98,6 @@ class SpriteSystem(System):
 
     def run(self):
         for each_subscribed in self.subscribers:
-            print("Painting")
             img = self.sprite_service.get_sprite(
                 each_subscribed.components[Sprite].sprite_name
             )
