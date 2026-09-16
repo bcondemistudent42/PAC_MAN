85b1e6fbfdcbe54bb5f5bb89ed6db2f9946708c6
Balthazar Condemine <you@example.com>
2026-09-16 14:57:00 +0200

    feat:devlopping mapping display coord into graph coord

--- src/pacman/engine/systems/defaults/target.py
+++ src/pacman/engine/systems/defaults/target.py
@@ -11,19 +11,24 @@ class TargetSystem(System):
 
     def run(self):
         # to name better the var to see later first make it work
-        print("TEST CALING")
         if self.first_run:
             for each_subscriber in self.subscribers:
                 behavior_component = each_subscriber.get_component(Target)
                 x = each_subscriber.get_component(Position).x
                 y = each_subscriber.get_component(Position).y
+                # ghosts_coord = (0, 0)
                 pac_man_coord = (10, 10)
-                self.behavior = (
+                astar =behavior_component.behavior(
                     (x, y),
                     pac_man_coord,
                     behavior_component.maze_size,
                     behavior_component.maze
                 )
-                road = self.behavior.find_pacman()
-                # print(road)to see latee how to do compatible
+                road = astar.find_pacman()
+                print(road)
+                # to see latee how to do compatible
 
+
+def pixel_to_coord(display_maze_size: tuple[int, int], graph_size: tuple[int, int]):
+    x_diplay, y_display = display_maze_size
+    x_graph, y_graph = graph_size
