c919635b4cd271033388fc872c26d087c93a1189
Anselme Bounouar <abounoua@student.42lyon.fr>
2026-09-21 21:47:13 +0200

    chore: removed some commets

--- src/pacman/__main__.py
+++ src/pacman/__main__.py
@@ -14,11 +14,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # try:
     main()
-# except Exception as e:
-# print(f"\n[ERROR]: {e}\n")
-
-
-# feat docs chore fix refactor
-# to scale all the images
--- src/pacman/parser.py
+++ src/pacman/parser.py
@@ -5,23 +5,22 @@ from pydantic import BaseModel, ConfigDict, Field
 
 
 class Parser(BaseModel):
-
     seed: int = Field(default=42, ge=0, le=1000)
     lives: int = Field(default=3, ge=1, le=100000)
-    level_max_time: int= Field(default=200, ge=30, le=100000)
-    point_per_pacgum: int= Field(default=10, ge=1, le=1000)
-    point_per_ghosts: int= Field(default=200, ge=1, le=1000)
-    points_per_super_pacgum: int= Field(default=50, ge=0, le=1000)
+    level_max_time: int = Field(default=200, ge=30, le=100000)
+    point_per_pacgum: int = Field(default=10, ge=1, le=1000)
+    point_per_ghosts: int = Field(default=200, ge=1, le=1000)
+    points_per_super_pacgum: int = Field(default=50, ge=0, le=1000)
 
-    model_config = ConfigDict(extra='forbid')
+    model_config = ConfigDict(extra="forbid")
+
 
 def parse():
     with open("config.json") as f:
         raw_data = f.read()
-        first_clean = re.sub(r'//.*', '', raw_data)
-        second_clean = re.sub(r'#.*', '', first_clean)
+        first_clean = re.sub(r"//.*", "", raw_data)
+        second_clean = re.sub(r"#.*", "", first_clean)
 
     cleaned_data = json.loads(second_clean)
     data = Parser.model_validate(cleaned_data)
     return data
-    # to handle later the try except properly in main with pydantic error and file not found error
